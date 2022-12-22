0899f7733eb1288395ac67aa597a1a53765b0704
unknown <BT_3W2_12@BKTECH.LOCAL>
2022-12-22 09:30:13 -0500

    Initial commit

---
+++ src/Cheese.java
@@ -0,0 +1,7 @@
+public class Cheese {
+    public static void main(String[] args) {
+       String str1 = "good morning";
+        int strLen = str1.length();
+        System.out.println(strLen);
+    }
+}
