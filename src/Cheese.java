4b51a5333b8543156a84f44edb82c8af1c509ca0
Gwimmm <ronaldoc25@nycstudents.net>
2023-01-05 09:31:28 -0500

    final code alost done

--- src/Cheese.java
+++ src/Cheese.java
@@ -1,7 +1,21 @@
+
+import java.util.Scanner;
 public class Cheese {
     public static void main(String[] args) {
-       String str1 = "good morning";
-        int strLen = str1.length();
-        System.out.println(strLen);
+        Scanner scan = new Scanner(System.in);
+        System.out.print("First String: ");
+        String str1 = scan.nextLine();
+
+        int length = str1.length();
+        System.out.println("String Length: " + length);
+
+
+        System.out.println("First Half: " + );
+        System.out.println("Second Half: " + );
+
+        // used later not rn.//
+        int idx1 = str1.indexOf(length/2);
+        int idx2 = str1.indexOf();
+
     }
 }
